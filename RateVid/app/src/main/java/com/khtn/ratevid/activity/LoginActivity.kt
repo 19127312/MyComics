63ab022a8c74a238ba2360b6aa15b084599a9372
19127324 <64062447+19127324@users.noreply.github.com>
2022-03-23 14:27:20 +0700

    Test RecycleVIew

--- RateVid/app/src/main/java/com/khtn/ratevid/activity/LoginActivity.kt
+++ RateVid/app/src/main/java/com/khtn/ratevid/activity/LoginActivity.kt
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.khtn.ratevid.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -14,7 +21,18 @@ class LoginActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_test)
+        var customListView = findViewById<RecyclerView>(R.id.recycle)
+        var studentList = ArrayList<Int>()
+        studentList.add(1)
+        studentList.add(1)
+        studentList.add(2)
+        studentList.add(1)
+        studentList.add(3)
+        var adapter = ContactsAdapter(studentList)
+        customListView?.adapter = adapter
+        customListView?.layoutManager = LinearLayoutManager(this)
+        /*
         registerTV.setOnClickListener {
             val intent= Intent(this, RegisterActivity::class.java)
             startActivity(intent)
@@ -30,7 +48,7 @@ class LoginActivity : AppCompatActivity() {
             }
             login(emailET.text.toString(),passwordET.text.toString())
         }
-        forgetPassword()
+        forgetPassword()*/
     }
 
     private fun forgetPassword() {
@@ -67,4 +85,35 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
+
+
+}
+class ContactsAdapter(private val studentList : ArrayList<Int>) :
+    RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
+
+        val StudentIcon = listItemView.findViewById<TextView>(R.id.textView)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val contactView = inflater.inflate(R.layout.test, parent, false)
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val a : Int = studentList.get(position)
+
+        var textView = holder.StudentIcon
+        textView.setText(a.toString())
+    }
+
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
+
+
 }
